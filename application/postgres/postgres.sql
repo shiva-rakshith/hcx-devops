72f7077201c7240966816918a1f2609b3e2b361e
Shiva Rakshith <80666319+shiva-rakshith@users.noreply.github.com>
2023-04-28 19:14:56 +0530

    feat: update query

--- application/postgres/postgres.sql
+++ application/postgres/postgres.sql
@@ -3,7 +3,7 @@
 CREATE TABLE IF NOT EXISTS mock_participant(
     parent_participant_code char varying , 
     child_participant_code char varying primary key, 
-    primary_email char varying
+    primary_email char varying,
     password char varying,
     private_key char varying
 );
