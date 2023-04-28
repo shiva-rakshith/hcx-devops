34ccf2f19d6204e9f23ab93da2335652e044c212
Shiva Rakshith <80666319+shiva-rakshith@users.noreply.github.com>
2023-04-28 19:19:06 +0530

    Update postgres.sql

--- application/postgres/postgres.sql
+++ application/postgres/postgres.sql
@@ -1,6 +1,6 @@
 \c postgres hcxpostgresql
 
-CREATE TABLE IF NOT EXISTS mock_participants(
+CREATE TABLE IF NOT EXISTS mock_participant(
     parent_participant_code char varying , 
     child_participant_code char varying primary key, 
     primary_email char varying,
